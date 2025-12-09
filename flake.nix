396036ffab41bb96c56557074675dda6df8ec310
oxey <lucoerlemans37@gmail.com>
2025-12-09 11:54:49 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Rust devshell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            rust
+            pkg-config
+          ];
+        };
+      }
+    );
+}
